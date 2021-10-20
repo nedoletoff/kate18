8a1789d442d708a69f6e5707b6d4dcd33620ce6b
nedoletoff <48565773+nedoletoff@users.noreply.github.com>
2021-10-20 17:39:08 +0300

    Update myset.hpp

--- myset.hpp
+++ myset.hpp
@@ -5,18 +5,12 @@
 class mySet : public std::set<int>{
 	public:
 		mySet combine(const mySet& a) {
-			mySet y;
-			y = *this;
-			clear();
-			std::set<int> c = a;	
-			for (auto ita = y.begin(); ita != y.end(); ++ita) {
-				for(auto itb = c.begin(); itb != c.end(); ++itb) {
-					if (*ita == *itb){
-						insert(*itb);
-					}
-				}
-			}
-			return *this;
+			mySet c;
+                        for (auto e1 : a)
+                                for (auto e2 : *this)
+                                        if (e1 == e2)
+                                                c.insert(e1);                                                                           *this = c;
+                        return *this;;
 		}
 		mySet intersect(const mySet& a) {
 			std::set<int> c = a;
